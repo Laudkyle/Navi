7dbb1bef0faa8c976d1f8bbef946afc7dfa78b5c
kyle <laudkyle@github.com>
2024-01-09 17:27:36 +0000

    Creating the Navi class

---
+++ navi.py
@@ -0,0 +1,8 @@
+# Classes
+class Navi():
+    def __init__(self,name,state,position,battery):
+        self.name = name
+        self.state = state
+        self.position = position
+        self.battery = battery
+    
