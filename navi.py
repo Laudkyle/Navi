e8621993b25299d72a858d5d4ca42bf72e741ecb
kyle <laudkyle@github.com>
2024-01-09 21:24:45 +0000

    add state change

--- navi.py
+++ navi.py
@@ -1,8 +1,18 @@
 # Classes
 class Navi():
-    def __init__(self,name,state,position,battery):
+    def __init__(self,name,currentState,battery):
         self.name = name
-        self.state = state
-        self.position = position
+        self.states = ['idle','recognition','navigation']
+        self.currentState = self.states[0]
         self.battery = battery
     
+    def changeState(self,state):
+        self.currentState =self.states[state]
+        
+class State():
+    def __init__(self,state,status):
+        self.state = state
+        self.status = status
+    
+    
+        
